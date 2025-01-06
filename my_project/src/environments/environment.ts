d55bd39d12861a6a6c0814977fe7182c1648256e
jalaj8009 <151819140+jalaj8009@users.noreply.github.com>
2025-01-06 18:32:16 +0530

    Update environment.ts

--- my_project/src/environments/environment.ts
+++ my_project/src/environments/environment.ts
@@ -1,10 +1,21 @@
-export interface EnvConfig {
-  apiUrl?: string;
-}
+// export interface EnvConfig {
+//   apiUrl?: string;
+// }
 
-const envConfig: EnvConfig = (window as any).__env || {};
+// const envConfig: EnvConfig = (window as any).__env || {};
+
+// export const environment = {
+//   production: false,
+//   apiUrl: envConfig.apiUrl || 'http://localhost:8000'
+// };
+
+
+
+import { loadEnvironmentConfig } from './environment.config';
+
+const envConfig = loadEnvironmentConfig();
 
 export const environment = {
   production: false,
-  apiUrl: envConfig.apiUrl || 'http://localhost:8000'
+  apiUrl: envConfig.apiUrl || 'http://localhost:8000', // Default to localhost if not provided
 };
