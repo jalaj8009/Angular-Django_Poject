13fbf1a696ff0cb0e35d5217cb4a3014d39c00c7
jalaj8009 <151819140+jalaj8009@users.noreply.github.com>
2025-01-06 19:02:44 +0530

    Update views.py

--- sop_django/ORSAPI/views.py
+++ sop_django/ORSAPI/views.py
@@ -1,3 +1,48 @@
+# from django.views.decorators.csrf import csrf_exempt
+# from .restctl.ChangepasswordCtl import ChangepasswordCtl
+# from .restctl.CollegeCtl import CollegeCtl
+# from .restctl.CourseCtl import CourseCtl
+# from .restctl.FacultyCtl import FacultyCtl
+# from .restctl.ForgetpasswordCtl import ForgetpasswordCtl
+# from .restctl.LoginCtl import LoginCtl
+# from .restctl.MarksheetCtl import MarksheetCtl
+# from .restctl.RegistrationCtl import RegistrationCtl
+# from .restctl.RoleCtl import RoleCtl
+# from .restctl.StudentCtl import StudentCtl
+# from .restctl.SubjectCtl import SubjectCtl
+# from .restctl.TimeTableCtl import TimeTableCtl
+# from .restctl.UserCtl import UserCtl
+# from .restctl.VehicleCtl import VehicleCtl
+# from .restctl.PortfolioManagementCtl import PortfolioManagementCtl
+# # from .restctl.ShoppingCartCtl import ShoppingCartCtl
+# # from .restctl.LeadCtl import LeadCtl
+# # from .restctl.OrderCtl import OrderCtl
+# # from .restctl.IssueCtl import IssueCtl
+# # from .restctl.DoctorCtl import DoctorCtl
+# from .restctl.TaskCtl import TaskCtl
+# from .restctl.StaffmemberCtl import StaffmemberCtl
+
+
+
+
+
+
+# # Create your view here
+# def info(request,page,action):
+#     print("Request Method -----",request.method)
+#     print("Page -----",page)
+#     print("Action -----", action)
+#     print("Base Path:---- ", __file__)
+
+
+# @csrf_exempt
+# def action(request,page,action="get",id=0,pageNo=1):
+#     print("ID ---",id)
+#     info(request,page,action)
+#     methodCall = page + "Ctl()." + action + "(request,{'id':id, 'pageNo':pageNo})"
+#     response = eval(methodCall)
+#     return response
+
 from django.views.decorators.csrf import csrf_exempt
 from .restctl.ChangepasswordCtl import ChangepasswordCtl
 from .restctl.CollegeCtl import CollegeCtl
@@ -14,33 +59,31 @@ from .restctl.TimeTableCtl import TimeTableCtl
 from .restctl.UserCtl import UserCtl
 from .restctl.VehicleCtl import VehicleCtl
 from .restctl.PortfolioManagementCtl import PortfolioManagementCtl
-# from .restctl.ShoppingCartCtl import ShoppingCartCtl
-# from .restctl.LeadCtl import LeadCtl
-# from .restctl.OrderCtl import OrderCtl
-# from .restctl.IssueCtl import IssueCtl
-# from .restctl.DoctorCtl import DoctorCtl
 from .restctl.TaskCtl import TaskCtl
 from .restctl.StaffmemberCtl import StaffmemberCtl
 
-
-
-
-
-
 # Create your view here
-def info(request,page,action):
-    print("Request Method -----",request.method)
-    print("Page -----",page)
+def info(request, page, action):
+    print("Request Method -----", request.method)
+    print("Page -----", page)
     print("Action -----", action)
     print("Base Path:---- ", __file__)
 
-
 @csrf_exempt
-def action(request,page,action="get",id=0,pageNo=1):
-    print("ID ---",id)
-    info(request,page,action)
-    methodCall = page + "Ctl()." + action + "(request,{'id':id, 'pageNo':pageNo})"
-    response = eval(methodCall)
-    return response
+def action(request, page, action="get", id=0, pageNo=1):
+    print("ID ---", id)
+    info(request, page, action)
 
+    # Build the method call dynamically
+    methodCall = f"{page}Ctl().{action}(request, {{'id': id, 'pageNo': pageNo}})"
+    
+    try:
+        # Dynamically execute the method call
+        response = eval(methodCall)
+    except NameError as e:
+        return HttpResponse(f"Error: {str(e)}", status=500)
+    except Exception as e:
+        return HttpResponse(f"An unexpected error occurred: {str(e)}", status=500)
+
+    return response
 
